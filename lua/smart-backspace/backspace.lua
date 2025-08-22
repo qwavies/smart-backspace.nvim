26647da3c78d4382bb99fff5162ccf5dd469d9a6
dtchen <dtchen247@gmail.com>
2025-08-22 20:32:28 +1000

    feat: removes extra indentation if previous line ends in a semicolon

--- lua/smart-backspace/backspace.lua
+++ lua/smart-backspace/backspace.lua
@@ -9,6 +9,24 @@ local function contains_only_whitespace(str)
    return true
 end
 
+local function count_whitepsace(str)
+   local tabs_to_spaces_ratio = vim.api.nvim_get_option_value("tabstop", { buf = 0 })
+   local count = 0
+
+   for char in str:gmatch(".") do
+      if char == " " then
+         count = count + 1
+      elseif char == "\t" then
+         count = count + tabs_to_spaces_ratio
+      else
+         -- non-whitespace character
+         return count
+      end
+   end
+
+   return count
+end
+
 local function remove_whitespace(cursor_pos, current_line)
    local row = cursor_pos[1] - 1
    local col = cursor_pos[2]
@@ -16,17 +34,28 @@ local function remove_whitespace(cursor_pos, current_line)
 
    local prev_line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
 
-   if (row > 0) and (contains_only_whitespace(prev_line)) then
-      -- edge case where line above is empty
+   if (row == 0) then
+      -- edge case: if on line 1, just delete from start to cursor
+      vim.api.nvim_buf_set_lines(0, 0, 1, false, {after_cursor})
+      vim.api.nvim_win_set_cursor(0, {1, 0})
+
+   elseif (row > 0) and (contains_only_whitespace(prev_line)) then
+      -- edge case: line above is empty
       vim.api.nvim_buf_set_lines(0, row - 1, row, false, {}) -- simply remove above line
+
+   elseif (prev_line:sub(-1) == ";") and (count_whitepsace(current_line) > count_whitepsace(prev_line)) then
+      -- edge case: above prev_line ends (denoted with ;) and current_line is over-indented
+      local prev_line_whitespace = prev_line:match("^(%s+)")
+      vim.api.nvim_buf_set_lines(0, row, row + 1, false, {prev_line_whitespace .. after_cursor})
+      vim.api.nvim_win_set_cursor(0, {row + 1, #prev_line_whitespace})
+
    elseif (row > 0) then
       local new_line = prev_line .. after_cursor
       vim.api.nvim_buf_set_lines(0, row - 1, row + 1, false, {new_line}) -- replace both lines w new_line
       vim.api.nvim_win_set_cursor(0, {row, #prev_line}) -- set cursor at end of previous line content
+
    else
-      -- if just on line 1, just delete from start to cursor
-      vim.api.nvim_buf_set_lines(0, 0, 1, false, {after_cursor})
-      vim.api.nvim_win_set_cursor(0, {1, 0})
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<BS>", true, false, true), "n", true)
    end
 end
 
