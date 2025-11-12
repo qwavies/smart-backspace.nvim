9857aa236dec610bad44dc6c8ffa05138867cbea
qwavies <qwavsbusiness@gmail.com>
2025-11-12 12:13:52 +1100

    chore: rewrite global variable condition to make it even more straightforward

--- plugin/smart-backspace.lua
+++ plugin/smart-backspace.lua
@@ -2,8 +2,7 @@ vim.defer_fn(function()
    if vim.g.smart_backspace_loaded then
       return
    else
+      require("smart-backspace").setup()
       vim.g.smart_backspace_loaded = true
    end
-
-   require("smart-backspace").setup()
 end, 10)
